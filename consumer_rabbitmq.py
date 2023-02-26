362cfb2537b2480807d741e674b92c510bc44683
mohtasimhadi <mohtasimhadi@gmail.com>
2023-02-26 17:22:58 +0600

    rabbitmq file queue updated

--- consumer_rabbitmq.py
+++ consumer_rabbitmq.py
@@ -2,16 +2,16 @@ import pika, sys, os, json
 from cib_analytics.api_generation.generate_full_response import generate_full_response
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq-temp1.centralindia.azurecontainer.io'))
     channel = connection.channel()
     
-    channel.queue_declare(queue='cib_ai_response')
+    channel.queue_declare(queue='response')
     
     def callback(ch, method, properties, body):
         print(f"received new message: {body}")
         channel.basic_publish(exchange='', routing_key='cib_analysis_report', body=json.dumps(generate_full_response()))
     
-    channel.basic_consume(queue='cib_ai_response', auto_ack=True, on_message_callback=callback)
+    channel.basic_consume(queue='response', auto_ack=True, on_message_callback=callback)
     channel.start_consuming()
 
 
