d53807904c0b0f781b7d414f8093d8b07625bb0d
mohtasimhadi <mohtasimhadi@gmail.com>
2023-03-09 14:11:28 +0600

    consumer data format

--- cib_analytics/api_generation/consumer_dashboard.py
+++ cib_analytics/api_generation/consumer_dashboard.py
@@ -1,26 +1,26 @@
 def get_consumer_dashboard():
     consumer_response = {
-        'Credit Facilities in the name of Applicant: (For Personal Loan / Car Loan / Home Loan)': [
+        'credit_facilities_is_the_name_of_applicant_for_personal_loan_car_loan_home_loan': [
             {
                 "FI Name": 'will be inputted manually',
                 "Borrower Name": 'Rizwanur Rahman',
-                "Applicant's Role": 'Borrower',
+                "Applicants Role": 'Borrower',
                 "Nature of Facility": 'Term Loan',
                 "Sanctioned Limit": '190000',
                 "Position Date": '2-10-2021',
                 "Outstanding": '12355',
                 "Overdue": '34729',
                 "CL Status": 'STD',
-                "EMI of Term Loan or % of Credit Card Limit / Outstanding": '9092',
+                "EMI of Term Loan or percent of Credit Card Limit Outstanding": '9092',
                 "Loan Start Date": '2-20-2020',
                 "Loan Expiry Date": '1-2-2025',
             },
         ],
-        'Credit Facilities in the name of Applicant: (For Credit Card)': [
+        'credit_facilities_in_the_name_of_applicant_for_credit_card': [
             {
                 "FI Name": 'will be inputted manually',
                 "Borrower Name": 'Rizwanur Rahman',
-                "Applicant's Role": 'Borrower',
+                "Applicants Role": 'Borrower',
                 "Nature of Facility": 'Credit card',
                 "Sanctioned Limit": '190000',
                 "Position Date": '2-10-2021',
@@ -28,24 +28,24 @@ def get_consumer_dashboard():
                 "Last 12 months Average of Credit Card Outstanding": '880325',
                 "Overdue": '3472956',
                 "CL Status": 'STD',
-                "EMI of Term Loan or % of Credit Card Limit / Outstanding": '9092',
+                "EMI of Term Loan or percent of Credit Card Limit Outstanding": '9092',
                 "Loan Start Date": '2-20-2020',
                 "Loan Expiry Date": '1-2-2025',
             },
         ],
-        "Credit Facilities in the name of Applicant's Business: (For Personal Loan / Car Loan / Home Loan / Credit Card)": [
+        "credit_facilities_in_the_name_of_applicants_business_for_personal_loan_car_loan_home_loan_credit_card": [
             {
                 "FI Name": 'will be inputted manually',
                 "Borrower Name": 'Rizwanur Rahman',
-                "Applicant's Role": 'Borrower',
+                "Applicants Role": 'Borrower',
                 "Nature of Facility": 'Term Loan',
                 "Limit": '190000',
                 "Position Date": '2-10-2021',
                 "Outstanding": '12355',
                 "Overdue": '34729',
                 "CL Status": 'STD',
-                "Interest Rate of CC-Hypo / OD (%)": '46758',
-                "EMI of Term Loan or Monthly Interest of CC/OD": '909223',
+                "Interest Rate of CC Hypo OD": '46758',
+                "EMI of Term Loan or Monthly Interest of CC OD": '909223',
                 "Loan Start Date": '2-20-2020',
                 "Loan Expiry Date": '1-2-2025',
             },
