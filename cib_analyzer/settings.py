e89abc7e4f45da5b33bd3794e2ec18bcb171a294
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2023-04-30 14:51:11 +0600

    django bug fix

--- cib_analyzer/settings.py
+++ cib_analyzer/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cib_analytics'
+    'cib_analytics',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
