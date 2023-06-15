4fa007248f936ac38f9517c1b45d1eadbfe2afcc
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2023-06-15 16:01:24 +0600

    initial structure of corporate

--- cib_analytics/corporate/corporate_class_summary_CIB_lIability.py
+++ cib_analytics/corporate/corporate_class_summary_CIB_lIability.py
@@ -24,3 +24,210 @@ class corporate_class_summary_CIB_liability:
             },
             "remarks": summary_engine.get_remarks(cib_data)
         }
+
+        self.other_sister_concern = {
+            "funded": {
+                "installment": summary_engine.funded_installment(cib_data),
+                "no_installment": summary_engine.funded_no_installment(cib_data),
+                "total": summary_engine.funded_total(cib_data)
+            },
+            "non_funded": summary_engine.non_funded_total(cib_data),
+            "total": summary_engine.total(cib_data),
+            "overdue": summary_engine.get_overdue(cib_data),
+            "cl_status": summary_engine.get_cl_status(cib_data),
+            "default": summary_engine.get_default(cib_data),
+            "loan_amount": {
+                "STD": summary_engine.get_std(cib_data),
+                "SMA": summary_engine.get_sma(cib_data),
+                "SS": summary_engine.get_ss(cib_data),
+                "DF": summary_engine.get_df(cib_data),
+                "BL": summary_engine.get_bl(cib_data),
+                "BLW": summary_engine.get_blw(cib_data),
+                "stay_order": summary_engine.get_stay_order(cib_data)
+            },
+            "remarks": summary_engine.get_remarks(cib_data)
+        }
+
+        self.other_concern_shareholder = {
+            "funded": {
+                "installment": summary_engine.funded_installment(cib_data),
+                "no_installment": summary_engine.funded_no_installment(cib_data),
+                "total": summary_engine.funded_total(cib_data)
+            },
+            "non_funded": summary_engine.non_funded_total(cib_data),
+            "total": summary_engine.total(cib_data),
+            "overdue": summary_engine.get_overdue(cib_data),
+            "cl_status": summary_engine.get_cl_status(cib_data),
+            "default": summary_engine.get_default(cib_data),
+            "loan_amount": {
+                "STD": summary_engine.get_std(cib_data),
+                "SMA": summary_engine.get_sma(cib_data),
+                "SS": summary_engine.get_ss(cib_data),
+                "DF": summary_engine.get_df(cib_data),
+                "BL": summary_engine.get_bl(cib_data),
+                "BLW": summary_engine.get_blw(cib_data),
+                "stay_order": summary_engine.get_stay_order(cib_data)
+            },
+            "remarks": summary_engine.get_remarks(cib_data)
+        }
+
+        self.directors = {
+            "funded": {
+                "installment": summary_engine.funded_installment(cib_data),
+                "no_installment": summary_engine.funded_no_installment(cib_data),
+                "total": summary_engine.funded_total(cib_data)
+            },
+            "non_funded": summary_engine.non_funded_total(cib_data),
+            "total": summary_engine.total(cib_data),
+            "overdue": summary_engine.get_overdue(cib_data),
+            "cl_status": summary_engine.get_cl_status(cib_data),
+            "default": summary_engine.get_default(cib_data),
+            "loan_amount": {
+                "STD": summary_engine.get_std(cib_data),
+                "SMA": summary_engine.get_sma(cib_data),
+                "SS": summary_engine.get_ss(cib_data),
+                "DF": summary_engine.get_df(cib_data),
+                "BL": summary_engine.get_bl(cib_data),
+                "BLW": summary_engine.get_blw(cib_data),
+                "stay_order": summary_engine.get_stay_order(cib_data)
+            },
+            "remarks": summary_engine.get_remarks(cib_data)
+        }
+
+        self.guarantor = {
+            "funded": {
+                "installment": summary_engine.funded_installment(cib_data),
+                "no_installment": summary_engine.funded_no_installment(cib_data),
+                "total": summary_engine.funded_total(cib_data)
+            },
+            "non_funded": summary_engine.non_funded_total(cib_data),
+            "total": summary_engine.total(cib_data),
+            "overdue": summary_engine.get_overdue(cib_data),
+            "cl_status": summary_engine.get_cl_status(cib_data),
+            "default": summary_engine.get_default(cib_data),
+            "loan_amount": {
+                "STD": summary_engine.get_std(cib_data),
+                "SMA": summary_engine.get_sma(cib_data),
+                "SS": summary_engine.get_ss(cib_data),
+                "DF": summary_engine.get_df(cib_data),
+                "BL": summary_engine.get_bl(cib_data),
+                "BLW": summary_engine.get_blw(cib_data),
+                "stay_order": summary_engine.get_stay_order(cib_data)
+            },
+            "remarks": summary_engine.get_remarks(cib_data)
+        }
+
+        self.corporate_guarantor = {
+            "funded": {
+                "installment": summary_engine.funded_installment(cib_data),
+                "no_installment": summary_engine.funded_no_installment(cib_data),
+                "total": summary_engine.funded_total(cib_data)
+            },
+            "non_funded": summary_engine.non_funded_total(cib_data),
+            "total": summary_engine.total(cib_data),
+            "overdue": summary_engine.get_overdue(cib_data),
+            "cl_status": summary_engine.get_cl_status(cib_data),
+            "default": summary_engine.get_default(cib_data),
+            "loan_amount": {
+                "STD": summary_engine.get_std(cib_data),
+                "SMA": summary_engine.get_sma(cib_data),
+                "SS": summary_engine.get_ss(cib_data),
+                "DF": summary_engine.get_df(cib_data),
+                "BL": summary_engine.get_bl(cib_data),
+                "BLW": summary_engine.get_blw(cib_data),
+                "stay_order": summary_engine.get_stay_order(cib_data)
+            },
+            "remarks": summary_engine.get_remarks(cib_data)
+        }
+
+        self.related_party_of_gurantor = {
+            "funded": {
+                "installment": summary_engine.funded_installment(cib_data),
+                "no_installment": summary_engine.funded_no_installment(cib_data),
+                "total": summary_engine.funded_total(cib_data)
+            },
+            "non_funded": summary_engine.non_funded_total(cib_data),
+            "total": summary_engine.total(cib_data),
+            "overdue": summary_engine.get_overdue(cib_data),
+            "cl_status": summary_engine.get_cl_status(cib_data),
+            "default": summary_engine.get_default(cib_data),
+            "loan_amount": {
+                "STD": summary_engine.get_std(cib_data),
+                "SMA": summary_engine.get_sma(cib_data),
+                "SS": summary_engine.get_ss(cib_data),
+                "DF": summary_engine.get_df(cib_data),
+                "BL": summary_engine.get_bl(cib_data),
+                "BLW": summary_engine.get_blw(cib_data),
+                "stay_order": summary_engine.get_stay_order(cib_data)
+            },
+            "remarks": summary_engine.get_remarks(cib_data)
+        }
+
+        self.total_of_related_party = {
+            "funded": {
+                "installment": summary_engine.funded_installment(cib_data),
+                "no_installment": summary_engine.funded_no_installment(cib_data),
+                "total": summary_engine.funded_total(cib_data)
+            },
+            "non_funded": summary_engine.non_funded_total(cib_data),
+            "total": summary_engine.total(cib_data),
+            "overdue": summary_engine.get_overdue(cib_data),
+            "cl_status": summary_engine.get_cl_status(cib_data),
+            "default": summary_engine.get_default(cib_data),
+            "loan_amount": {
+                "STD": summary_engine.get_std(cib_data),
+                "SMA": summary_engine.get_sma(cib_data),
+                "SS": summary_engine.get_ss(cib_data),
+                "DF": summary_engine.get_df(cib_data),
+                "BL": summary_engine.get_bl(cib_data),
+                "BLW": summary_engine.get_blw(cib_data),
+                "stay_order": summary_engine.get_stay_order(cib_data)
+            },
+            "remarks": summary_engine.get_remarks(cib_data)
+        }
+
+        self.others = {
+            "funded": {
+                "installment": summary_engine.funded_installment(cib_data),
+                "no_installment": summary_engine.funded_no_installment(cib_data),
+                "total": summary_engine.funded_total(cib_data)
+            },
+            "non_funded": summary_engine.non_funded_total(cib_data),
+            "total": summary_engine.total(cib_data),
+            "overdue": summary_engine.get_overdue(cib_data),
+            "cl_status": summary_engine.get_cl_status(cib_data),
+            "default": summary_engine.get_default(cib_data),
+            "loan_amount": {
+                "STD": summary_engine.get_std(cib_data),
+                "SMA": summary_engine.get_sma(cib_data),
+                "SS": summary_engine.get_ss(cib_data),
+                "DF": summary_engine.get_df(cib_data),
+                "BL": summary_engine.get_bl(cib_data),
+                "BLW": summary_engine.get_blw(cib_data),
+                "stay_order": summary_engine.get_stay_order(cib_data)
+            },
+            "remarks": summary_engine.get_remarks(cib_data)
+        }
+
+        self.total = {
+            "funded": {
+                "installment": summary_engine.funded_installment(cib_data),
+                "no_installment": summary_engine.funded_no_installment(cib_data),
+                "total": summary_engine.funded_total(cib_data)
+            },
+            "non_funded": summary_engine.non_funded_total(cib_data),
+            "total": summary_engine.total(cib_data),
+            "overdue": summary_engine.get_overdue(cib_data),
+            "cl_status": summary_engine.get_cl_status(cib_data),
+            "default": summary_engine.get_default(cib_data),
+            "loan_amount": {
+                "STD": summary_engine.get_std(cib_data),
+                "SMA": summary_engine.get_sma(cib_data),
+                "SS": summary_engine.get_ss(cib_data),
+                "DF": summary_engine.get_df(cib_data),
+                "BL": summary_engine.get_bl(cib_data),
+                "BLW": summary_engine.get_blw(cib_data),
+                "stay_order": summary_engine.get_stay_order(cib_data)
+            },
+            "remarks": summary_engine.get_remarks(cib_data)
+        }
