486f1f84ce2fc6702300931e2a55254bd1fbc819
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2023-07-05 17:34:19 +0600

    rabbitmq host changed

--- consumer_rabbitmq.py
+++ consumer_rabbitmq.py
@@ -17,7 +17,7 @@ def main():
     #connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq-temp1.centralindia.azurecontainer.io', heartbeat=400))
     
     #production
-    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbit@rabbitmq-0.rabbitmq.rabbits.svc.cluster.local', heartbeat=400))
+    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq-0.rabbitmq.rabbits.svc.cluster.local', heartbeat=400))
     
     channel = connection.channel()
     channel.queue_declare(queue='prime_bank_cib_response')
--- image.sh
+++ image.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-imageName=dharapat-prime-bank-cib:prod_v1.0
+imageName=dharapat-prime-bank-cib:prod_v1.7
 
 echo [LOG] Building docker image...
 docker build --network=host -t $imageName -f DockerFile  .
+docker docker buildx build --platform linux/amd64,linux/arm64 --network=host -t $imageName -f DockerFile  .
 
 echo [LOG] Tagging and pushing docker image to container registry...
 docker tag $imageName dharapat.azurecr.io/$imageName
