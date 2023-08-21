948570d625b9c35aa74dbad1008fd0a9ec7e7682
HomairaSoheny <98115036+HomairaSoheny@users.noreply.github.com>
2023-08-21 12:39:41 +0600

    Update reschedule_loan_summary_class.py

--- cib_analytics/corporate/reschedule_loan_summary_class.py
+++ cib_analytics/corporate/reschedule_loan_summary_class.py
@@ -1,6 +1,6 @@
 from .engine import reschedule_loan_summary
 
-class reschedule_loan_summary_table_class:
+class RescheduleLoanSummaryTableClass:
     def __init__(self, cib_list):
         self.Reschedule_loan_for_borrower = reschedule_loan_summary.rescheduled_loan_borrow(cib_list)
         self.Reschedule_loan_for_gurantor = reschedule_loan_summary.rescheduled_loan_guran(cib_list)
