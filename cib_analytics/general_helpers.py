893d82f7342232f0dfcb8834721a1a38d817c7f4
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2023-08-26 16:58:23 +0600

    pad to dict_list for df conversion for xlsx

--- cib_analytics/general_helpers.py
+++ cib_analytics/general_helpers.py
@@ -59,3 +59,13 @@ def cib_class_division(cib_list):
         return cibs
     except:
         print("Error dividing the tables")
+
+def pad_dict_list(dict_list, padel):
+    lmax = 0
+    for lname in dict_list.keys():
+        lmax = max(lmax, len(dict_list[lname]))
+    for lname in dict_list.keys():
+        ll = len(dict_list[lname])
+        if  ll < lmax:
+            dict_list[lname] += [padel] * (lmax - ll)
+    return dict_list
