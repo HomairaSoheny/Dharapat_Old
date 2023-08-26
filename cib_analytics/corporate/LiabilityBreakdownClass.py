e92443a8ea90c08bd1592a0157bf875e78d2b2bf
HomairaSoheny <98115036+HomairaSoheny@users.noreply.github.com>
2023-08-27 01:03:59 +0600

    Update LiabilityBreakdownClass.py
    Added a new function to extract the name of borrowing company or person

--- cib_analytics/corporate/LiabilityBreakdownClass.py
+++ cib_analytics/corporate/LiabilityBreakdownClass.py
@@ -2,7 +2,7 @@ from .engine import liability_breakdown
 
 class LiabilityBreakdown:
     def __init__(self, cib_list):
-        self.response = {
+        self.response = { 
             "Borrowing Company or Person": liability_breakdown.borrowing_com_per(cib_list),
             "A Overdraft CC  OD Cash Credit": liability_breakdown.a_overdraft(cib_list),
             "Overdue EOL of A": liability_breakdown.a_overdue(cib_list),
