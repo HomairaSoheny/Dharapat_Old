6400693ff22e0cbce04146dca7a29fa83427abdb
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2023-10-04 00:46:20 +0600

    formated xlsx for consumer

--- cib_analytics/spreadsheet_generation/consumer_spreadsheet.py
+++ cib_analytics/spreadsheet_generation/consumer_spreadsheet.py
@@ -8,19 +8,19 @@ def generate_consumer_spreadsheet(writer, cib):
     
     row, col = 8, 2
     
-    basic_info.to_excel(writer, sheet_name="Consumer Report", startrow=2, startcol=2, index=False)
-    writer.sheets["Consumer Report"].merge_range("C3:D3", "CIB Informantion")
+    basic_info.to_excel(writer, sheet_name=cib["basic_info"]["CIB Report of"], startrow=2, startcol=2, index=False)
+    writer.sheets[cib["basic_info"]["CIB Report of"]].merge_range("C3:D3", "CIB Informantion")
     
-    writer.sheets["Consumer Report"].merge_range("C8:R8", "Credit facilities in the name of applicant: (For Personal Loan/Car Loan/Home Loan)")
-    credit_card_loan.to_excel(writer, sheet_name="Consumer Report", startrow=row, startcol=col, index=False)
+    writer.sheets[cib["basic_info"]["CIB Report of"]].merge_range("C8:R8", "Credit facilities in the name of applicant: (For Personal Loan/Car Loan/Home Loan)")
+    credit_card_loan.to_excel(writer, sheet_name=cib["basic_info"]["CIB Report of"], startrow=row, startcol=col, index=False)
     row += credit_card_loan.shape[0]+3
     
-    writer.sheets["Consumer Report"].merge_range("C"+str(row)+":R"+str(row), "Credit facilities in the name of applicant: (For Credit Card)")
-    personal_loan.to_excel(writer, sheet_name="Consumer Report", startrow=row, startcol=col, index=False)
+    writer.sheets[cib["basic_info"]["CIB Report of"]].merge_range("C"+str(row)+":R"+str(row), "Credit facilities in the name of applicant: (For Credit Card)")
+    personal_loan.to_excel(writer, sheet_name=cib["basic_info"]["CIB Report of"], startrow=row, startcol=col, index=False)
     row += personal_loan.shape[0]+3
     
-    writer.sheets["Consumer Report"].merge_range("C"+str(row)+":R"+str(row), "Credit facilities in the name of applicant's business: (For Car Loan/Home Loan/Credit Card)")
-    business_loan.to_excel(writer, sheet_name="Consumer Report", startrow=row, startcol=col, index=False)
+    writer.sheets[cib["basic_info"]["CIB Report of"]].merge_range("C"+str(row)+":R"+str(row), "Credit facilities in the name of applicant's business: (For Car Loan/Home Loan/Credit Card)")
+    business_loan.to_excel(writer, sheet_name=cib["basic_info"]["CIB Report of"], startrow=row, startcol=col, index=False)
     row += personal_loan.shape[0]
     for col_idx in range(row):
-        writer.sheets["Consumer Report"].set_column(col_idx, col_idx, 40)
+        writer.sheets[cib["basic_info"]["CIB Report of"]].set_column(col_idx, col_idx, 25)
