6a88ba0d1395dd1c92db91eeee0e7b0cbd7ee316
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2024-02-01 21:21:21 +0600

    consumer api created

--- cib_analytics/api_generation/generate_full_response.py
+++ cib_analytics/api_generation/generate_full_response.py
@@ -1,12 +1,13 @@
 from .consumer_dashboard import get_consumer_dashboard
 from .corporate_dashboard import get_corporate_dashboard
+from consumer.consumer import getConsumerDashboard
 
 def generate_full_response(cib_list, cib_type):
     response = []
     if cib_type == 'corporate':
         response.append(get_corporate_dashboard(cib_list))
     else:
-        for cib in cib_list:
-            response.append(get_consumer_dashboard(cib))
-            
+        response.append(getConsumerDashboard(cib_list))
+        # for cib in cib_list:
+        #     response.append(get_consumer_dashboard(cib))
     return response
