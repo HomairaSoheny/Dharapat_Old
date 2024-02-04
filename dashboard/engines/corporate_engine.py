5d68d08afdcf209a32b41f47ef6f3953164fd804
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2024-02-04 17:14:23 +0600

    Corporate DataFrame

--- dashboard/engines/corporate_engine.py
+++ dashboard/engines/corporate_engine.py
@@ -1,3 +1,6 @@
+import pandas as pd
+from dashboard.engines import general_engine
+
 def getCIBCategory(cib):
     category_mapping = {
         "Type a": "Concerns of primary borrower with PBL",
@@ -52,4 +55,44 @@ def getDateOfLastPayment(fac):
     for key in ['Date of last payment']:
         if key in fac['Ref'].keys():
             return fac['Ref'][key]
-        
+
+def getCorporateDataFrame(cibs):
+    df = pd.DataFrame()
+    for cib in cibs:
+        for fac_type in (cib.installment_facility, cib.noninstallment_facility, cib.credit_card_facility):
+            response = []
+            if fac_type is not None:
+                for fac in fac_type:
+                    response.append({
+                        "CIB Category": getCIBCategory(cib),
+                        "Name": general_engine.getBorrowersName(cib.subject_info),
+                        "Facility Type": general_engine.getFacilityType(fac),
+                        "Phase": general_engine.getPhase(fac),
+                        "Role": general_engine.getRole(fac),
+                        "Is Non Funded": general_engine.isNonFunded(fac),
+                        "Facility Type": "Installment/No Installment (Not Implemented)",
+                        "Outstanding": general_engine.getOutstanding(fac),
+                        "Overdue": general_engine.getOverdue(fac),
+                        "CL Status": general_engine.getCurrentCLStatus(fac),
+                        "Default": "Not Implemented",
+                        "Limit": general_engine.getLimit(fac),
+                        "Loan/Limit (days of adjustment before/after)": "Need elaboration",
+                        "Worse Classification Status": general_engine.getWorstCLStatus(fac),
+                        "Date of Classification": getDateOfClassification(fac),
+                        "Start Date": getStartDate(fac),
+                        "End Date of Contract": getEndDateOfContract(fac),
+                        "Type of Reschedule": "Need Elaboration",
+                        "Reschedule Amount": "Need Elaboration",
+                        "Date of Last Rescheduling": "Need Elaboration",
+                        "Requested Amount": "Need Elaboration",
+                        "Date of Request": "Need Elaboration",
+                        "Writ no": "Need Elaboration",
+                        "Remarks": getRemarks(fac),
+                        "Payment Period (Monthly/Quarterly)": getPaymentPeriod(fac),
+                        "Total No of Installment": getTotalNumberOfInstallment(fac),
+                        "No of Remaining Installment": getNoOfRemainingInstallment(fac),
+                        "Date of Last Payment": getDateOfLastPayment(fac),
+                        "NPL": "Need Elaboration"
+                    })
+            df = pd.concat([df, pd.DataFrame(response)], ignore_index=True)
+    return df
