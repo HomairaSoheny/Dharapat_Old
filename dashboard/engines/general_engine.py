9da2c9eaa6cea184b07d99eaf025b75edbc3317d
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2024-02-04 15:52:01 +0600

    funding status of concern

--- dashboard/engines/general_engine.py
+++ dashboard/engines/general_engine.py
@@ -1,3 +1,10 @@
+def isNonFunded(fac):
+    keywords = ['non funded', 'letter of credit', 'gurantee', 'other indirect facility']
+    for key in keywords:
+        if key in fac["Ref"]["Facility"].lower():
+            return True
+    return False
+
 def getBorrowersName(subject_info):
     keys = ['Title', 'Name', 'Title, Name', 'Trade Name']
     for key in keys:
