ac983d0418249a337805662f0a179cb132648543
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2024-02-12 10:18:35 +0600

    summary table function renovated

--- dashboard/corporate.py
+++ dashboard/corporate.py
@@ -1,5 +1,4 @@
 from dashboard.engines.corporate_engine import *
-from dashboard.engines import general_engine
 
 def getSummaryTable(df):
     response = []
@@ -7,49 +6,13 @@ def getSummaryTable(df):
         cat_df = df[df['CIB Category'] == category]
         for concern_name in df['Name'].unique():
             temp_df = cat_df[cat_df['Name'] == concern_name]
-            response.append({
-                "CIB Category": category,
-                "Name of Concern": concern_name,
-                "Funded Outstanding Installment": getFundedOutstandingInstallment(temp_df),
-                "Funded Outstanding Non Installment": getFundedOutstandingNonInstallment(temp_df),
-                "Funded Outstanding Total": getFundedOutstandingTotal(temp_df),
-                "Non-Funded Outstanding": getNonFundedOutstanding(temp_df),
-                "Total Outstanding": getTotalOutstanding(temp_df),
-                "Overdue": getOverdue(temp_df),
-                "CL Status": general_engine.getClassFromSet(set(temp_df['CL Status'].tolist())),
-                "Default": "Yes" if "Yes" in set(temp_df['Default'].tolist()) else "No",
-                "Updated Overdue and CL Status": "Need More Clarification"
-            })
+            response.append(getSummaryTableFields(category, concern_name, temp_df))
         sub_total_df = pd.DataFrame(response)
         sub_total_df = sub_total_df[sub_total_df['CIB Category'] == category]
-        response.append({
-            "CIB Category": category,
-            "Name of Concern": "Sub Total",
-            "Funded Outstanding Installment": float(format(sub_total_df['Funded Outstanding Installment'].sum(), '.3f')),
-            "Funded Outstanding Non Installment": float(format(sub_total_df['Funded Outstanding Non Installment'].sum(), '.3f')),
-            "Funded Outstanding Total": float(format(sub_total_df['Funded Outstanding Total'].sum(), '.3f')),
-            "Non-Funded Outstanding": float(format(sub_total_df['Non-Funded Outstanding'].sum(), '.3f')),
-            "Total Outstanding": float(format(sub_total_df['Total Outstanding'].sum(), '.3f')),
-            "Overdue": float(format(sub_total_df['Overdue'].sum(), '.3f')),
-            "CL Status": general_engine.getClassFromSet(set(sub_total_df['CL Status'].tolist())),
-            "Default": "Yes" if "Yes" in set(sub_total_df['Default'].tolist()) else "No",
-            "Updated Overdue and CL Status": "Need More Clarification"
-        })
+        response.append(getSummaryTableSum(category, "Sub Total", sub_total_df))
     total_df = pd.DataFrame(response)
     total_df = total_df[total_df['Name of Concern'] == "Sub Total"]
-    response.append({
-            "CIB Category": category,
-            "Name of Concern": "Grand Total",
-            "Funded Outstanding Installment": float(format(total_df['Funded Outstanding Installment'].sum(), '.3f')),
-            "Funded Outstanding Non Installment": float(format(total_df['Funded Outstanding Non Installment'].sum(), '.3f')),
-            "Funded Outstanding Total": float(format(total_df['Funded Outstanding Total'].sum(), '.3f')),
-            "Non-Funded Outstanding": float(format(total_df['Non-Funded Outstanding'].sum(), '.3f')),
-            "Total Outstanding": float(format(total_df['Total Outstanding'].sum(), '.3f')),
-            "Overdue": float(format(sub_total_df['Overdue'].sum(), '.3f')),
-            "CL Status": general_engine.getClassFromSet(set(total_df['CL Status'].tolist())),
-            "Default": "Yes" if "Yes" in set(total_df['Default'].tolist()) else "No",
-            "Updated Overdue and CL Status": "Need More Clarification"
-        })
+    response.append(getSummaryTableSum(category, "Grand Total", total_df))
     return response
 
 def getSummaryOfTerminatedFacilityFunded(df):
--- dashboard/engines/corporate_engine.py
+++ dashboard/engines/corporate_engine.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from dashboard.engines import general_engine
 
+
 def getCIBCategory(cib):
     category_mapping = {
         "Type a": "Concerns of primary borrower with PBL",
@@ -11,122 +12,190 @@ def getCIBCategory(cib):
         "Type f": "Guarantors CIB (Personal)",
         "Type g": "Corporate Guarantor",
         "Type h": "Related party of Guarantor",
-        "Type i": "Other concerns/persons not related to the company"
+        "Type i": "Other concerns/persons not related to the company",
     }
 
     return category_mapping.get(cib.cib_category, None)
 
+
 def getDateOfClassification(fac):
-    for key in ['Date of classification']:
-        if key in fac['Ref'].keys():
-            return fac['Ref'][key]
-        
+    for key in ["Date of classification"]:
+        if key in fac["Ref"].keys():
+            return fac["Ref"][key]
+
+
 def getStartDate(fac):
-    for key in ['Starting date']:
-        if key in fac['Ref'].keys():
-            return fac['Ref'][key]
-        
+    for key in ["Starting date"]:
+        if key in fac["Ref"].keys():
+            return fac["Ref"][key]
+
+
 def getEndDateOfContract(fac):
-    for key in ['End date of contract']:
-        if key in fac['Ref'].keys():
-            return fac['Ref'][key]
-        
+    for key in ["End date of contract"]:
+        if key in fac["Ref"].keys():
+            return fac["Ref"][key]
+
+
 def getRemarks(fac):
-    for key in ['Remarks']:
-        if key in fac['Ref'].keys():
-            return fac['Ref'][key]
-        
+    for key in ["Remarks"]:
+        if key in fac["Ref"].keys():
+            return fac["Ref"][key]
+
+
 def getPaymentPeriod(fac):
-    for key in ['Payments Periodicity', 'Payments periodicity',]:
-        if key in fac['Ref'].keys():
-            return fac['Ref'][key]
+    for key in [
+        "Payments Periodicity",
+        "Payments periodicity",
+    ]:
+        if key in fac["Ref"].keys():
+            return fac["Ref"][key]
+
 
 def getTotalNumberOfInstallment(fac):
-    for key in ['Total number of installments']:
-        if key in fac['Ref'].keys():
-            return fac['Ref'][key]
-        
+    for key in ["Total number of installments"]:
+        if key in fac["Ref"].keys():
+            return fac["Ref"][key]
+
+
 def getNoOfRemainingInstallment(fac):
-    for key in ['Remaining installments Number']:
-        if key in fac['Ref'].keys():
-            return fac['Ref'][key]
-        
+    for key in ["Remaining installments Number"]:
+        if key in fac["Ref"].keys():
+            return fac["Ref"][key]
+
+
 def getDateOfLastPayment(fac):
-    for key in ['Date of last payment']:
-        if key in fac['Ref'].keys():
-            return fac['Ref'][key]
+    for key in ["Date of last payment"]:
+        if key in fac["Ref"].keys():
+            return fac["Ref"][key]
+
 
 def getFacilityType(i):
-    facility_types = {
-        0: "Installment",
-        1: "No Installment",
-        2: "Credit Card"
-    }
+    facility_types = {0: "Installment", 1: "No Installment", 2: "Credit Card"}
     return facility_types.get(i, "Unknown")
 
+
 def getFundedOutstandingInstallment(df):
-    df = df[df['Is Funded'] == 'Yes']
-    df = df[df['Installment Type'] == 'Installment']
-    return float(format(df['Outstanding'].sum()/1000000, ".3f"))
+    df = df[df["Is Funded"] == "Yes"]
+    df = df[df["Installment Type"] == "Installment"]
+    return float(format(df["Outstanding"].sum() / 1000000, ".3f"))
+
 
 def getFundedOutstandingNonInstallment(df):
-    df = df[df['Is Funded'] == 'Yes']
-    df = df[df['Installment Type'] == 'No Installment']
-    return float(format(df['Outstanding'].sum()/1000000, ".3f"))
+    df = df[df["Is Funded"] == "Yes"]
+    df = df[df["Installment Type"] == "No Installment"]
+    return float(format(df["Outstanding"].sum() / 1000000, ".3f"))
+
 
 def getFundedOutstandingTotal(df):
-    return float(format(getFundedOutstandingInstallment(df) + getFundedOutstandingNonInstallment(df), '.3f'))
+    return float(
+        format(
+            getFundedOutstandingInstallment(df)
+            + getFundedOutstandingNonInstallment(df),
+            ".3f",
+        )
+    )
+
 
 def getNonFundedOutstanding(df):
-    df = df[df['Is Funded'] == 'No']
-    return float(format(df['Outstanding'].sum()/1000000, ".3f"))
+    df = df[df["Is Funded"] == "No"]
+    return float(format(df["Outstanding"].sum() / 1000000, ".3f"))
+
 
 def getTotalOutstanding(df):
-    return float(format(getFundedOutstandingTotal(df) + getNonFundedOutstanding(df), '.3f'))
+    return float(
+        format(getFundedOutstandingTotal(df) + getNonFundedOutstanding(df), ".3f")
+    )
+
 
 def getOverdue(df):
-    return float(format(df['Overdue'].sum()/1000000, ".3f"))
+    return float(format(df["Overdue"].sum() / 1000000, ".3f"))
+
 
 def getDefault(fac):
-    return "Yes" if "Yes" in fac['Contract History']['Default'].tolist() else "No"
+    return "Yes" if "Yes" in fac["Contract History"]["Default"].tolist() else "No"
+
+
+def getSummaryTableFields(category, concern_name, df):
+    return {
+        "CIB Category": category,
+        "Name of Concern": concern_name,
+        "Funded Outstanding Installment": getFundedOutstandingInstallment(df),
+        "Funded Outstanding Non Installment": getFundedOutstandingNonInstallment(df),
+        "Funded Outstanding Total": getFundedOutstandingTotal(df),
+        "Non-Funded Outstanding": getNonFundedOutstanding(df),
+        "Total Outstanding": getTotalOutstanding(df),
+        "Overdue": getOverdue(df),
+        "CL Status": general_engine.getClassFromSet(set(df["CL Status"].tolist())),
+        "Default": "Yes" if "Yes" in set(df["Default"].tolist()) else "No",
+        "Updated Overdue and CL Status": "Need More Clarification",
+    }
+
+
+def getSummaryTableSum(category, concern_name, df):
+    return {
+        "CIB Category": category,
+        "Name of Concern": concern_name,
+        "Funded Outstanding Installment": float(format(df["Funded Outstanding Installment"].sum(), ".3f")),
+        "Funded Outstanding Non Installment": float(format(df["Funded Outstanding Non Installment"].sum(), ".3f")),
+        "Funded Outstanding Total": float(format(df["Funded Outstanding Total"].sum(), ".3f")),
+        "Non-Funded Outstanding": float(format(df["Non-Funded Outstanding"].sum(), ".3f")),
+        "Total Outstanding": float(format(df["Total Outstanding"].sum(), ".3f")),
+        "Overdue": float(format(df["Overdue"].sum(), ".3f")),
+        "CL Status": general_engine.getClassFromSet(set(df["CL Status"].tolist())),
+        "Default": "Yes" if "Yes" in set(df["Default"].tolist()) else "No",
+        "Updated Overdue and CL Status": "Need More Clarification",
+    }
+
 
 def getCorporateDataFrame(cibs):
     df = pd.DataFrame()
     for cib in cibs:
-        for i, fac_type in enumerate((cib.installment_facility, cib.noninstallment_facility, cib.credit_card_facility)):
+        for i, fac_type in enumerate(
+            (
+                cib.installment_facility,
+                cib.noninstallment_facility,
+                cib.credit_card_facility,
+            )
+        ):
             response = []
             if fac_type is not None:
                 for fac in fac_type:
-                    response.append({
-                        "CIB Category": getCIBCategory(cib),
-                        "Name": general_engine.getBorrowersName(cib.subject_info),
-                        "Facility Type": general_engine.getFacilityType(fac),
-                        "Phase": general_engine.getPhase(fac),
-                        "Role": general_engine.getRole(fac),
-                        "Is Funded": general_engine.isFunded(fac),
-                        "Installment Type": getFacilityType(i),
-                        "Outstanding": general_engine.getOutstanding(fac),
-                        "Overdue": general_engine.getOverdue(fac),
-                        "CL Status": general_engine.getCurrentCLStatus(fac),
-                        "Default": getDefault(fac),
-                        "Limit": general_engine.getLimit(fac),
-                        "Loan/Limit (days of adjustment before/after)": "Need elaboration",
-                        "Worse Classification Status": general_engine.getWorstCLStatus(fac),
-                        "Date of Classification": getDateOfClassification(fac),
-                        "Start Date": getStartDate(fac),
-                        "End Date of Contract": getEndDateOfContract(fac),
-                        "Type of Reschedule": "Need Elaboration",
-                        "Reschedule Amount": "Need Elaboration",
-                        "Date of Last Rescheduling": "Need Elaboration",
-                        "Requested Amount": "Need Elaboration",
-                        "Date of Request": "Need Elaboration",
-                        "Writ no": "Need Elaboration",
-                        "Remarks": getRemarks(fac),
-                        "Payment Period (Monthly/Quarterly)": getPaymentPeriod(fac),
-                        "Total No of Installment": getTotalNumberOfInstallment(fac),
-                        "No of Remaining Installment": getNoOfRemainingInstallment(fac),
-                        "Date of Last Payment": getDateOfLastPayment(fac),
-                        "NPI": general_engine.getCurrentNPI(fac)
-                    })
+                    response.append(
+                        {
+                            "CIB Category": getCIBCategory(cib),
+                            "Name": general_engine.getBorrowersName(cib.subject_info),
+                            "Facility Type": general_engine.getFacilityType(fac),
+                            "Phase": general_engine.getPhase(fac),
+                            "Role": general_engine.getRole(fac),
+                            "Is Funded": general_engine.isFunded(fac),
+                            "Installment Type": getFacilityType(i),
+                            "Outstanding": general_engine.getOutstanding(fac),
+                            "Overdue": general_engine.getOverdue(fac),
+                            "CL Status": general_engine.getCurrentCLStatus(fac),
+                            "Default": getDefault(fac),
+                            "Limit": general_engine.getLimit(fac),
+                            "Loan/Limit (days of adjustment before/after)": "Need elaboration",
+                            "Worse Classification Status": general_engine.getWorstCLStatus(
+                                fac
+                            ),
+                            "Date of Classification": getDateOfClassification(fac),
+                            "Start Date": getStartDate(fac),
+                            "End Date of Contract": getEndDateOfContract(fac),
+                            "Type of Reschedule": "Need Elaboration",
+                            "Reschedule Amount": "Need Elaboration",
+                            "Date of Last Rescheduling": "Need Elaboration",
+                            "Requested Amount": "Need Elaboration",
+                            "Date of Request": "Need Elaboration",
+                            "Writ no": "Need Elaboration",
+                            "Remarks": getRemarks(fac),
+                            "Payment Period (Monthly/Quarterly)": getPaymentPeriod(fac),
+                            "Total No of Installment": getTotalNumberOfInstallment(fac),
+                            "No of Remaining Installment": getNoOfRemainingInstallment(
+                                fac
+                            ),
+                            "Date of Last Payment": getDateOfLastPayment(fac),
+                            "NPI": general_engine.getCurrentNPI(fac),
+                        }
+                    )
             df = pd.concat([df, pd.DataFrame(response)], ignore_index=True)
     return df
