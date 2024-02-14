4d986f725315e5bb8da656a7382dfe14cf51b67d
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2024-02-14 12:35:58 +0600

    Report log message

--- report/report_download.py
+++ report/report_download.py
@@ -5,11 +5,14 @@ from report.excel.consumer import generateConsumerSpreadsheet
 from report.excel.corporate import generateCorporateSpreadsheet
 
 def createReportDashboard(analysis_report):
+    print("Entered report dashboard")
     io = BytesIO()
     writer = pd.ExcelWriter(io, engine='xlsxwriter', )
     if type(analysis_report) == list:
+        print("Generating Consumer Analysis Report")
         generateConsumerSpreadsheet(writer, analysis_report)
     else:
+        print("Generating Corporate Analysis Report")
         generateCorporateSpreadsheet(writer, analysis_report)
     
     return writer, io
--- report/views.py
+++ report/views.py
@@ -2,23 +2,28 @@ from django.http import HttpResponse
 from rest_framework.views import APIView
 from report.report_download import createReportDashboard
 import ast
+from beeprint import pp
 import json
 import os
 abs_path = os.path.dirname(os.path.abspath(__file__))
 
 class GeneralDashboardReportApiView(APIView):
     def post(self, request):
-        try:
-            body_unicode = request.body.decode('utf-8')
-            body = ast.literal_eval(body_unicode)
-            writer, io = createReportDashboard(body)
-            writer.close()
-            
-            rFile = io.getvalue()
-            response = HttpResponse(rFile,content_type="application/ms-excel")
-            response['Content-Disposition'] = f'attachment; filename=data.xlsx'
+        # try:
+        print("Analysis report received")
+        body_unicode = request.body.decode('utf-8')
+        body = ast.literal_eval(body_unicode)
+        print("\n\n___________________________________________________\n\n")
+        pp(body)
+        print("\n\n___________________________________________________\n\n")
+        writer, io = createReportDashboard(body)
+        writer.close()
+        
+        rFile = io.getvalue()
+        response = HttpResponse(rFile,content_type="application/ms-excel")
+        response['Content-Disposition'] = f'attachment; filename=data.xlsx'
 
-            return HttpResponse(response)
-        except Exception as exc:
-             print(exc)
-             return HttpResponse([exc])
+        return HttpResponse(response)
+        # except Exception as exc:
+        #      print(exc)
+        #      return HttpResponse([exc])
