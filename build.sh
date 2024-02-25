cb7ff76bc0063d87a84e99d6d2d2a2f7bda504a6
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2024-02-25 16:48:47 +0600

    UAT Ready

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-imageName=dharapat-prime-bank-cib:uat_v3.0.3
+imageName=dharapat-prime-bank-cib:uat_v3.0.4
 
 echo [LOG] Building docker image...
 docker buildx build --platform linux/x86_64 --network=host -t $imageName -f DockerFile  .
