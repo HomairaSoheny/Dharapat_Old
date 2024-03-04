aea6925db0e58cbebaf2cc4eaa97410ec098ebf2
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2024-03-04 11:52:01 +0600

    create_image test

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-imageName=dharapat-prime-bank-cib:uat_v3.0.5
+# imageName=dharapat-prime-bank-cib:uat_v3.0.5
+imageName=dharapat-prime-bank-cib:uat_v3.0.5_test
 
 echo [LOG] Building docker image...
 docker buildx build --platform linux/x86_64 --network=host -t $imageName -f DockerFile  .
